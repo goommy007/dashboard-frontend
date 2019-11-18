273387a006e5835995da30efde5b324f0e801a08
goommy007 <goommy@gmail.com>
2019-11-18 09:02:44 -0600

    Updates

---
+++ precache-manifest.a958f49279df8934fc1222c409561bd4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a25bcfee6e566fef094ee9c1f9a1a462",
+    "url": "/goommy007/dashboard-frontend/index.html"
+  },
+  {
+    "revision": "4e74df49f62e124e3391",
+    "url": "/goommy007/dashboard-frontend/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "b386c8d297898483daea",
+    "url": "/goommy007/dashboard-frontend/static/js/2.59bacf1b.chunk.js"
+  },
+  {
+    "revision": "4e74df49f62e124e3391",
+    "url": "/goommy007/dashboard-frontend/static/js/main.59b73823.chunk.js"
+  },
+  {
+    "revision": "7e70ad4cd8f6f2721237",
+    "url": "/goommy007/dashboard-frontend/static/js/runtime-main.af35d7be.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/goommy007/dashboard-frontend/static/media/logo.25bf045c.svg"
+  }
+]);

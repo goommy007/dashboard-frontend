37e4d6af36dfb74b128e81ee30ded8d09bff0cb0
goommy007 <goommy@gmail.com>
2019-11-18 10:46:37 -0600

    Modified Files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import React, { Component } from 'react';
 import NavBar from './NavBar/NavBar';
-import Admin from './Administrator/Admin';
-import Home from './NewUpdate/NewUpdate';
-import { Route, Switch } from 'react-router-dom';
-import Alert from './Questions/Alert';
+
 
 class App extends Component {
 
@@ -12,11 +9,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <NavBar />
         <h2 align="center">GREYHOUND STATUS MONITOR</h2>
-        <div align="center"><Alert /> </div>
-        <div align="center" ><Switch><Route path="/Admin" component={Admin} /></Switch> </div>
-        <div align="center" ><Switch><Route path="/Home" component={Home} /></Switch> </div>
       </div>
     );
   }
